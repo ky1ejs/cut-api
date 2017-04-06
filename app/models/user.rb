6835beb47ca126f7c5e5d8a9d645bf85f3569dab
Kyle McAlpine <k@kylejm.io>
2017-04-06 18:37:31 +0100

    Correcting callback used for setting initial last_seen

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :devices
   has_many :watch_list, foreign_key: :user_id, class_name: "WantToWatch"
 
-  before_create :set_initial_last_seen
+  before_validation :set_initial_last_seen, on: :create
   protected
   def set_initial_last_seen
     self.last_seen ||= Time.now
