9041028c7aa4fc5b6f00df9097fccd54d703e663
Kyle McAlpine <k@kylejm.io>
2017-04-10 08:53:44 +0100

    Adding through to has_many in User for convenience

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
   has_many :devices
-  has_many :watch_list, foreign_key: :user_id, class_name: "WantToWatch"
-  has_many :followers, foreign_key: :following_id, class_name: "Follow"
-  has_many :following, foreign_key: :follower_id, class_name: "Follow"
+  has_many :watch_list_records, foreign_key: :user_id, class_name: "WantToWatch"
+  has_many :watch_list, through: :watch_list_records, source: :film
+  has_many :follower_records, foreign_key: :following_id, class_name: "Follow"
+  has_many :following_records, foreign_key: :follower_id, class_name: "Follow"
+  has_many :followers, through: :follower_records
+  has_many :following, through: :following_records
 
   def can_login
     self.email != nil
