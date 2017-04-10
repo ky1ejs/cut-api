e80dd085f9a2fee0244213028fa2b5b332f7f00d
Kyle McAlpine <k@kylejm.io>
2017-04-10 14:33:33 +0100

    Corrected method used in film_controller

--- app/controllers/film_controller.rb
+++ app/controllers/film_controller.rb
@@ -2,12 +2,12 @@ class FilmController < ApplicationController
   def index
     FlixsterController.new.fetch_popular
 
-    watch_films_ids = device.user.watch_list.map { |watch| watch.film.id }
+    film_ids = device.user.watch_list.map { |film| film.id }
     all_films_json = Film.all.as_json(include: :posters)
 
     i = 0
     while i < all_films_json.count  do
-      all_films_json[i]['want_to_watch'] = watch_films_ids.include? all_films_json[i]['id']
+      all_films_json[i]['want_to_watch'] = film_ids.include? all_films_json[i]['id']
       i +=1
     end
 
