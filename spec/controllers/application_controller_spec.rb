ac27e843fb82e85ccb3147787b05efcfa4abf596
Kyle McAlpine <k@kylejm.io>
2017-04-11 07:08:04 +0100

    Fixing application_controller tests

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,25 +1,20 @@
 require "rails_helper"
 
 RSpec.describe ApplicationController, :type => :controller do
-  controller do
-    # def device
-    #   subject.device
-    # end
-  end
-
   it "saves given UUID and type" do
-    routes.draw { get "device" => "anonymous#device" }
-
     type = 'ios'
     uuid = '2a2096ff-ed6c-450d-bdb4-98757793fdeb'
 
     # expect(Device.find(uuid).nil?).to eq true
 
-    get :device, :HTTP_DEVICE_ID => "#{type}_#{uuid}"
+    @request.env['HTTP_DEVICE_ID'] = "#{type}_#{uuid}"
 
-    d = Device.find_by(uuid: uuid)
+    d = subject.device
 
     expect(d.id).to eq uuid
     expect(d.type).to eq type
+    expect(d.new_record?).to eq false
+
+    d.destroy!
   end
 end
