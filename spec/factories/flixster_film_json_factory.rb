f0d67aa3e8181f5c111204fe16dfe2dcdb32fa9a
Kyle McAlpine <k@kylejm.io>
2017-05-01 17:21:08 +0100

    Using FactoryGirl for flixster json

---
+++ spec/factories/flixster_film_json_factory.rb
@@ -0,0 +1,64 @@
+# factory :poster_json, class: Hash do
+#   size :small
+#   url "https://test.com/image.jpg"
+# end
+FactoryGirl.define do
+  factory :flixster_film_json, class: Hash do
+    skip_create
+
+    transient do
+      poster_size           { :small }
+      poster_url            { "https://test.com/image.jpg" }
+      user_score_count      { 1000 }
+      user_score            { 89 }
+      rotten_tomatoes_score { 83 }
+    end
+
+    id "771382349"
+    title "Interstellar"
+    runningTime "1 hr. 37 min."
+    year 2017
+    theaterReleaseDate {{
+      :year => "2017",
+      :month => "4",
+      :day => "07"
+    }}
+    trailer {{
+      "low" => "http://link.theplatform.com/s/NGweTC/media/iTHXNgNW4Lz5",
+      "med" => "http://link.theplatform.com/s/NGweTC/media/iTHXNgNW4Lz5",
+      "high" => "http://link.theplatform.com/s/NGweTC/media/iTHXNgNW4Lz5",
+      "hd" => "http://link.theplatform.com/s/NGweTC/media/iTHXNgNW4Lz5",
+      "thumbnail" => "http://resizing.flixster.com/QxyMqPE0tz_WSS8OJWwggnh_158=/171x128/v1.bjsxNDc5ODUzO2o7MTcyOTc7MjA0ODsxMDgwOzE5MjA",
+      "duration" => 151
+    }}
+    status "Live"
+    playing true
+    actors {[
+      {
+        "id": 162656441,
+        "name": "Alec Baldwin"
+      },
+      {
+        "id": 162652875,
+        "name": "Steve Buscemi"
+      },
+      {
+        "id": 351528089,
+        "name": "Jimmy Kimmel"
+      }
+    ]}
+    synopsis "Sweet ass movie"
+    poster {{ poster_size => poster_url }}
+    reviews {{
+      :flixster => {
+        :numScores => user_score_count,
+        :popcornScore => user_score
+      },
+      :rottenTomatoes => {
+        :rating => rotten_tomatoes_score
+      }
+    }}
+
+    initialize_with { attributes }
+  end
+end
--- spec/models/film_spec.rb
+++ spec/models/film_spec.rb
@@ -1,13 +1,15 @@
 require "rails_helper"
 
 RSpec.describe Film, :type => :model do
+
+
   it "parses a movie from Flixster" do
     title = "Interstellar"
     running_time = 97
     running_time_description = "1 hr. 37 min."
     theater_release_date = Date.new(2017, 4, 7)
     poster_url = "http://poster.com/image.jpg"
-    poster_size = 'thumbnail'
+    poster_size = :thumbnail
     synopsis = "Best movie ever"
 
     flixster_num_of_scores = 1134
@@ -15,59 +17,21 @@ RSpec.describe Film, :type => :model do
 
     rotten_tomatoes_score = 50
 
-    json = {
-      "id": 771382349,
-      "title": title,
-      "mpaa": "US: PG",
-      "runningTime": running_time_description,
-      "year": 2017,
-      "boxOffice": "",
-      "releaseType": "TopBoxOffice",
-      "theaterReleaseDate": {
-        "year": "2017",
-        "month": "4",
-        "day": "07"
+    json = create(:flixster_film_json,
+      title: title,
+      runningTime: running_time_description,
+      theaterReleaseDate: {
+        :year => theater_release_date.year.to_s,
+        :month => theater_release_date.month.to_s,
+        :day => theater_release_date.day.to_s
       },
-      "poster": { "#{poster_size}": poster_url },
-      "trailer": {
-        "low": "http://link.theplatform.com/s/NGweTC/media/iTHXNgNW4Lz5",
-        "med": "http://link.theplatform.com/s/NGweTC/media/iTHXNgNW4Lz5",
-        "high": "http://link.theplatform.com/s/NGweTC/media/iTHXNgNW4Lz5",
-        "hd": "http://link.theplatform.com/s/NGweTC/media/iTHXNgNW4Lz5",
-        "thumbnail": "http://resizing.flixster.com/QxyMqPE0tz_WSS8OJWwggnh_158=/171x128/v1.bjsxNDc5ODUzO2o7MTcyOTc7MjA0ODsxMDgwOzE5MjA",
-        "duration": 151
-      },
-      "status": "Live",
-      "playing": true,
-      "boxOfficeCurrencySymbol": "&#163;",
-      "actors": [
-        {
-          "id": 162656441,
-          "name": "Alec Baldwin"
-        },
-        {
-          "id": 162652875,
-          "name": "Steve Buscemi"
-        },
-        {
-          "id": 351528089,
-          "name": "Jimmy Kimmel"
-        }
-      ],
-      "synopsis": synopsis,
-      "reviews": {
-        "flixster": {
-          "numScores": flixster_num_of_scores,
-          "numWantToSee": 9818,
-          "popcornScore": flxster_user_score
-        },
-        "rottenTomatoes": {
-          "rating": rotten_tomatoes_score,
-          "certifiedFresh": false
-        }
-      }
-    }.symbolize_keys
-
+      synopsis: synopsis,
+      poster_size: poster_size,
+      poster_url: poster_url,
+      user_score_count: flixster_num_of_scores,
+      user_score: flxster_user_score,
+      rotten_tomatoes_score: rotten_tomatoes_score
+    )
 
     f = Film.from_flixster(json)
     f.save
@@ -76,7 +40,7 @@ RSpec.describe Film, :type => :model do
     expect(f.running_time).to eq running_time
     expect(f.theater_release_date).to eq theater_release_date
     expect(f.posters.first.url).to eq poster_url
-    expect(f.posters.first.size).to eq poster_size
+    expect(f.posters.first.size).to eq poster_size.to_s
     expect(f.synopsis).to eq synopsis
 
     expect(f.ratings.count).to eq 2
@@ -92,5 +56,61 @@ RSpec.describe Film, :type => :model do
     f.destroy
 
     expect(f.ratings.count).to eq 0
+  end
+
+  it "updates from flixster json" do
+    f = Film.new
+    f.title = "Test title"
+    f.save!
+
+    title = "Interstellar"
+    running_time = 97
+    running_time_description = "1 hr. 37 min."
+    theater_release_date = Date.new(2017, 4, 7)
+    poster_url = "http://poster.com/image.jpg"
+    poster_size = :thumbnail
+    synopsis = "Best movie ever"
+
+    flixster_num_of_scores = 1134
+    flxster_user_score = 63
+
+    rotten_tomatoes_score = 50
+
+    json = create(:flixster_film_json,
+      title: title,
+      runningTime: running_time_description,
+      theaterReleaseDate: {
+        :year => theater_release_date.year.to_s,
+        :month => theater_release_date.month.to_s,
+        :day => theater_release_date.day.to_s
+      },
+      synopsis: synopsis,
+      poster_size: poster_size,
+      poster_url: poster_url,
+      user_score_count: flixster_num_of_scores,
+      user_score: flxster_user_score,
+      rotten_tomatoes_score: rotten_tomatoes_score
+    )
+
+    f.update_with_flixster_json json
+
+    expect(f.title).to eq title
+    expect(f.running_time).to eq running_time
+    expect(f.theater_release_date).to eq theater_release_date
+    expect(f.posters.first.url).to eq poster_url
+    expect(f.posters.first.size).to eq poster_size.to_s
+    expect(f.synopsis).to eq synopsis
+
+    expect(f.ratings.count).to eq 2
+
+    expect(f.ratings[0].rating).to eq rotten_tomatoes_score / Float(100)
+    expect(f.ratings[0].rating_count).to eq nil
+    expect(f.ratings[0].source).to eq :rotten_tomatoes.to_s
+
+    expect(f.ratings[1].rating).to eq flxster_user_score / Float(100)
+    expect(f.ratings[1].rating_count).to eq flixster_num_of_scores
+    expect(f.ratings[1].source).to eq :flixster_users.to_s
+
+    f.destroy!
   end
 end
