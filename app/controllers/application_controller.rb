bca4991066e4b304e503d333eac760de07105c29
Kyle McAlpine <k@kylejm.io>
2017-05-06 16:44:34 +0100

    Correcting headers printing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::API
   def device
     regex = /^(?<platform>[a-z]*)_(?<id>[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12})$/i
-    puts request.headers
+    request.headers.each do |k, v|
+      puts "#{k} - #{v}"
+    end
     device_id = request.headers[:HTTP_DEVICE_ID].match regex
 
     is_dev_device = request.headers[:HTTP_IS_DEV_DEVICE]
