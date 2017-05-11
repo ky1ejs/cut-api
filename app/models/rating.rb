52f324a6511ff495da229f5513df61d08f40fd89
Kyle McAlpine <k@kylejm.io>
2017-05-11 08:39:07 +0100

    Adding validation for Rating

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,4 +1,7 @@
 class Rating < ApplicationRecord
   enum source: [ :flixster_users, :rotten_tomatoes, :imdb_users, :metacritic, :metacritic_users ]
+
   belongs_to :film
+
+  validates :score, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 1 }
 end
