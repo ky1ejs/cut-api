9f03eb4729204a1ef5affcab26312f1d4a36bf0e
Kyle McAlpine <k@kylejm.io>
2017-05-26 21:39:23 +0100

    Only sending notifications when they're new

--- app/models/notifications/notification.rb
+++ app/models/notifications/notification.rb
@@ -2,9 +2,18 @@ class Notification < ApplicationRecord
   belongs_to :user
 
   def save
+    is_new = new_record?
     return if !super
-    user.devices.each do |d|
-      NotificationService.publish(message, d)
+    if is_new
+      user.devices.each { |d| Rails.logger.debug NotificationService.publish(message, d) }
+    end
+  end
+
+  def save!
+    is_new = new_record?
+    return if !super
+    if is_new
+      user.devices.each { |d| Rails.logger.debug NotificationService.publish(message, d) }
     end
   end
 end
