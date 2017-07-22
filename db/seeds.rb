4239782eab495b995d98fb8e6e6676769b2a2ed6
Kyle McAlpine <k@kylejm.io>
2017-07-22 19:12:09 +0100

    Adding basic seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+def create_user(username)
+  u = User.create(username: username, password: 'Password123', email: "#{username}@cut.watch")
+  u.save!
+  u
+end
+
+kyle = create_user('kylejm')
+denny = create_user('denny')
+arun = create_user('arun')
+fabio = create_user('fabio')
+
+fabio.followers = [kyle, denny, arun]
+arun.followers = [denny, fabio]
+denny.followers = [fabio]
+kyle.followers = [fabio]
+
+spider_man = Film.create(title: "Spider-Man: Homecoming",
+                        theater_release_date: Date.new + 5.days,
+                        synopsis: "A young Peter Parker/Spider-Man (Tom Holland), who...")
+spider_man.save!
+
+inception = Film.create(title: "Inception",
+                        theater_release_date: Date.new + 5.days,
+                        synopsis: "Pretty much the best film ever")
+inception.save!
+
+watch = [
+  Watch.create(user: kyle, film: inception, rating: 5),
+  Watch.create(user: kyle, film: spider_man, rating: 4.0),
+
+  Watch.create(user: denny, film: spider_man),
+  Watch.create(user: denny, film: inception),
+
+  Watch.create(user: fabio, film: spider_man, rating: 4.5),
+  Watch.create(user: fabio, film: inception, rating: 4.0),
+
+  Watch.create(user: arun, film: spider_man),
+  Watch.create(user: arun, film: inception, rating: 4.0)
+]
+watch.each { |e| e.save! }
