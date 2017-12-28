d8e666910ca79a6f837a8ee3335353dd72bcfb84
Kyle McAlpine <k@kylejm.io>
2017-12-28 19:51:32 +0000

    Adding headers for each post_release command

--- post_release.rb
+++ post_release.rb
@@ -2,8 +2,24 @@ system "echo '###########'"
 system "echo 'ENVIRONMENT'"
 system 'echo $ENVIRONMENT'
 system "echo '###########'"
+system "echo ''"
+system "echo ''"
+system "echo '###########'"
+system "echo 'Run bundle exec rake db:migrate'"
+system "echo '###########'"
 system 'bundle exec rake db:migrate'
 if ENV['ENVIRONMENT']&.uppercase != 'PRODUCTION'
+  system "echo ''"
+  system "echo ''"
+  system "echo '###########'"
+  system "echo 'Run bundle exec rake db:seed'"
+  system "echo '###########'"
   system 'bundle exec rake db:seed'
+
+  system "echo ''"
+  system "echo ''"
+  system "echo '###########'"
+  system "echo 'Run bundle exec rake films:fetch_flixster'"
+  system "echo '###########'"
   system 'bundle exec rake films:fetch_flixster'
 end
