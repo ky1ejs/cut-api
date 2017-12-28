7e113b5850f6a7e33a5503f8ad2284cfb88e2274
Kyle McAlpine <k@kylejm.io>
2017-12-28 19:04:11 +0000

    Adding some debug to post_release.rb

--- post_release.rb
+++ post_release.rb
@@ -1,4 +1,8 @@
 exec 'bundle exec rake db:migrate'
+puts '###########'
+puts 'ENV'
+puts ENV['ENVIRONMENT']
+puts '###########'
 if ENV['ENVIRONMENT']&.uppercase != 'PRODUCTION'
   exec 'bundle exec rake db:seed'
   exec 'bundle exec rake films:fetch_flixster'
