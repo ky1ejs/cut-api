e37885bcb43391f5bc528b2ae07434bb10fe9912
Kyle McAlpine <k@kylejm.io>
2017-12-31 14:22:10 +0000

    Customising Film index and filters

--- app/admin/films.rb
+++ app/admin/films.rb
@@ -12,4 +12,47 @@ ActiveAdmin.register Film do
 #   permitted
 # end
 
+  index do
+    column :title
+    column :running_time
+    column :created_at
+    column :updated_at
+    column :posters do |film|
+      film.posters.count
+    end
+  end
+
+  show do
+    attributes_table do
+      row :title
+      row :synopsis
+      row :running_time
+      row :theater_release_date
+      row :created_at
+      row :updated_at
+      row :posters do |film|
+        film.posters.count
+      end
+    end
+
+    panel "Posters" do
+      table_for film.posters do
+        column :url
+        column :width
+        column :height
+      end
+    end
+  end
+
+  filter :title, as: :string
+  filter :running_time
+  filter :created_at
+  filter :updated_at
+  filter :poster_count, as: :numeric
+end
+
+class Film
+  ransacker :poster_count do
+    Arel.sql("(SELECT COUNT(*) FROM posters WHERE posters.film_id = films.id)")
+  end
 end
