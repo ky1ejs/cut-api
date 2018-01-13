3114d45b01070f8c60173e0204855bb728bb7a2d
Kyle McAlpine <k@kylejm.io>
2018-01-13 20:56:20 +0000

    Adding ability to update films using Flixster in Active Admin

--- app/admin/films.rb
+++ app/admin/films.rb
@@ -28,6 +28,20 @@ ActiveAdmin.register Film do
     link_to 'Flixster Data', "#{resource.id}/flixster_data"
   end
 
+  member_action :update_film, method: :post do
+    provider = FilmProvider.find_by(film_id: resource.id, provider: :flixster)
+    if provider.nil?
+      redirect_to "/admin/films/#{resource.id}", notice: "Flixster provider not found"
+    else
+      FlixsterController.new.update_or_create_film_with_id provider.provider_film_id
+      redirect_to "/admin/films/#{resource.id}", notice: "Your event was added"
+    end
+  end
+
+  action_item :update, only: :show  do
+    link_to "Update Films", "#{resource.id}/update_film", method: :post
+  end
+
   show do
     attributes_table do
       row :title
@@ -41,11 +55,21 @@ ActiveAdmin.register Film do
       end
     end
 
-    panel "Posters" do
+    panel 'Posters' do
       table_for film.posters do
         column :url
         column :width
         column :height
+      end
+    end
+
+    panel 'Providers' do
+      table_for film.providers do
+        column :provider
+        column :film_id
+        column :provider_film_id
+        column :created_at
+        column :updated_at
       end
     end
   end
