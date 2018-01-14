57875bb081a08a9140f31dac9e27bb98fd38ce73
Kyle McAlpine <k@kylejm.io>
2018-01-14 22:04:54 +0000

    Adding data and test push action to User and Device

--- app/admin/devices.rb
+++ app/admin/devices.rb
@@ -12,4 +12,31 @@ ActiveAdmin.register Device do
 #   permitted
 # end
 
+  show do
+    attributes_table do
+      row :user
+      row :platform
+      row :last_seen
+      row :is_dev_device
+      row :name
+      row :app
+      row :push_token
+      row :created_at
+      row :updated_at
+    end
+  end
+
+  member_action :send_test_push, method: :post do
+    if resource.push_token.nil?
+      redirect_to "/admin/devices/#{resource.id}", notice: "No push token"
+    else
+      NotificationService.send_message "Testing push token", resource
+      redirect_to "/admin/devices/#{resource.id}", notice: "Test sent"
+    end
+  end
+
+  action_item :test_push, only: :show  do
+    link_to "Test Push", "#{resource.id}/send_test_push", method: :post
+  end
+
 end
--- app/admin/users.rb
+++ app/admin/users.rb
@@ -12,4 +12,42 @@ ActiveAdmin.register User do
 #   permitted
 # end
 
+  show do
+    attributes_table do
+      row :username
+      row :last_seen
+      row :created_at
+      row :updated_at
+      row :devices do |user|
+        user.devices.count
+      end
+      row :ratings do |user|
+        user.rated_list.count
+      end
+      row :want_to_watches do |user|
+        user.want_to_watch_list.count
+      end
+    end
+
+    panel 'Devices' do
+      table_for user.devices do
+        column :platform
+      end
+    end
+
+    panel 'Ratings' do
+      table_for user.rated_list do
+        column :film do |watch|
+          link_to watch.film.title, "/admin/films/#{watch.film.id}"
+        end
+        column :rating do |watch|
+          watch.rating
+        end
+        column :comment
+        column :created_at
+        column :updated_at
+      end
+    end
+  end
+
 end
